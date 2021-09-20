57ee97f5360f8ed2adaf1c884c60bb270c82df16
Ana C <apspfc@gmail.com>
2021-09-20 14:23:24 +0100

    done

--- app.js
+++ app.js
@@ -9,11 +9,41 @@ require('./db');
 // https://www.npmjs.com/package/express
 const express = require('express');
 
+
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
 const hbs = require('hbs');
 
 const app = express();
+
+const session = require("express-session");
+app.use(
+    session({
+   
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        sameSite: true,
+        httpOnly: true,
+         
+    },
+
+})
+);
+
+function getCurrentLoggedUser(req, res, next) {
+    if(req.session && req.session.currentUser) {
+        app.locals.loggedInUser = req.session.currentUser.username;
+    } else {
+        app.locals.loggedInUser = "";
+    }
+    next();
+}
+
+app.use(getCurrentLoggedUser);
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
@@ -27,6 +57,9 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
+const auth = require("./routes/auth")
+app.use("/", auth);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 const router = require("express").Router();
 
+
+
+function validationTest(req, res, next) {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get("/main", validationTest, (req, res, next) => {
+  res.render("main");
+});
+
+
+router.get("/private", validationTest, (req, res, next) => {
+  res.render("private");
+});
+
 
 module.exports = router;
